50dc91119bf98d7ed3bca99ba3df49d137068f9b
qianhao <ianloe666@gmail.com>
2020-10-02 22:45:05 -0500

    Updates

---
+++ precache-manifest.54a1ca89a80f1ebe428c8dc31f9fcde4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8029d0cfbf85046d5fd86d624f4cecf9",
+    "url": "/TE-book-keeper/index.html"
+  },
+  {
+    "revision": "32bc2cdd435df3d42462",
+    "url": "/TE-book-keeper/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "1d4853c373e9817fe3fa",
+    "url": "/TE-book-keeper/static/css/main.dd0bb94c.chunk.css"
+  },
+  {
+    "revision": "32bc2cdd435df3d42462",
+    "url": "/TE-book-keeper/static/js/2.b78e1b80.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/TE-book-keeper/static/js/2.b78e1b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d4853c373e9817fe3fa",
+    "url": "/TE-book-keeper/static/js/main.58cee2c0.chunk.js"
+  },
+  {
+    "revision": "a619aef247691a3a5975",
+    "url": "/TE-book-keeper/static/js/runtime-main.03c8c4cb.js"
+  }
+]);
